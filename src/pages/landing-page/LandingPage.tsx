0c94d574a2bab910dd80e642c6e26fd564eb18a0
KendallRey <kendallreymozo@gmail.com>
2023-09-10 11:28:56 +0800

    added initial landing page

---
+++ src/pages/landing-page/LandingPage.tsx
@@ -0,0 +1,12 @@
+import style from './LandingPage.module.scss'
+
+const LandingPage = () => {
+
+    return (
+        <div className={style.container}>
+
+        </div>
+    )
+}
+
+export default LandingPage
