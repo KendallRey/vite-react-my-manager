82df76d13a89ed36fec34b3e92d9d6680ab0ca6b
KendallRey <kendallreymozo@gmail.com>
2023-09-10 11:29:55 +0800

    update firebase auth api

---
+++ src/firebase/api/auth/AuthApi.ts
@@ -0,0 +1,34 @@
+import { signInWithEmailAndPassword, signOut } from "@firebase/auth"
+import { auth } from "../../FirebaseManager"
+
+type SignInApiType = {
+    email : string
+    password : string
+}
+
+const SignInApi = async ({
+    email,
+    password,
+} : SignInApiType) => {
+    return await signInWithEmailAndPassword(auth, email, password)
+    .then((res) => {
+        return res
+    })
+    .catch((err) => {
+        return null
+    })
+}
+
+export default SignInApi
+
+export const SignOutApi = async () => {
+
+    return await signOut(auth)
+    .then((res) => {
+        return res
+    })
+    .catch((err) => {
+        return null
+    })
+}
+
