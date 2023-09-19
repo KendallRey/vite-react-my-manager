c019e6cd3994f4ec3d100dae857c19a9669f393f
KendallRey <107897863+KendallRey@users.noreply.github.com>
2023-09-19 14:52:25 +0800

    test -> linking existing account not working yet but saving for reference

--- src/firebase/api/auth/AuthProviderApi.ts
+++ src/firebase/api/auth/AuthProviderApi.ts
@@ -1,4 +1,5 @@
-import { GoogleAuthProvider, signInWithPopup, FacebookAuthProvider, TwitterAuthProvider, GithubAuthProvider, getAuth, Auth } from "firebase/auth";
+import { GoogleAuthProvider, signInWithPopup, FacebookAuthProvider, TwitterAuthProvider, GithubAuthProvider, getAuth, Auth, fetchSignInMethodsForEmail, EmailAuthProvider, signInWithCredential, linkWithPopup, AuthProvider } from "firebase/auth";
+import { auth } from "../../FirebaseManager";
 
 const GOOGLE_AUTH_PROVIDER = new GoogleAuthProvider()
 const FACEBOOK_PROVIDER = new FacebookAuthProvider()
@@ -38,12 +39,37 @@ export const SignInWithTwitter = async () => {
 
 
 export const SignInWithGithub = async () => {
+	
 	return await signInWithPopup(getAuth(), GITHUB_PROVIDER)
 	.then((res) => {
 		return res
 	})
-	.catch((err) => {
-		console.log("GIT_ERR", err)
+	.catch((err) => LinkAccoutIfExistingFromError(err, GITHUB_PROVIDER))
+}
+
+const LinkAccoutIfExistingFromError = async (err: any, provider : AuthProvider) => {
+
+	if(!err.hasOwnProperty("code")) return null
+	if(!err.hasOwnProperty("customData")) return null
+	if(!err.hasOwnProperty("name")) return null
+	if(!err.customData.hasOwnProperty("email")) return null
+
+	try {
+
+		const user_providers = await fetchSignInMethodsForEmail(auth, err.customData.email)
+
+		if(!user_providers) return null
+
+		const has_gmail = user_providers.includes("google.com")
+		if(!has_gmail) return null
+
+		const google_account = await SignInWithGoogle()
+		if(!google_account) return null
+
+		return await linkWithPopup(google_account.user, provider)
+
+	}
+	catch(e){
 		return null
-	})
+	}
 }
