107e1e8f52503ae9704a8b53b1381e8b9098ac00
KendallRey <107897863+KendallRey@users.noreply.github.com>
2023-09-22 16:20:13 +0800

    update, login page error display

--- src/pages/landing-page/LandingPage.tsx
+++ src/pages/landing-page/LandingPage.tsx
@@ -7,15 +7,16 @@ import Button from '../../component/button/Button'
 import { useState } from 'react'
 import { CleanText } from '../../utils/DataHandler'
 import { useNavigate } from 'react-router-dom'
+import BulletList from '../../component/bullet-list/BulletList'
 
 const LandingPage = () => {
 
 	const navigate = useNavigate()
 
-	// const { user, setUser } = useFirebaseAuth()
+	const [isLoading, setIsLoading] = useState(false)
 
 	// 
-	//#region Login 
+	//#region Login Date on Change
 
 	const [loginData, setLoginData] = useState({
 		email : "",
@@ -25,20 +26,39 @@ const LandingPage = () => {
 	const onChangeLogin = (e : RCE<HTMLInputElement>) => {
 		const { name, value } = e.target
 		const clean_value = CleanText(value)
+		setErrors([])
 		setLoginData(prev => ({...prev, [name] : clean_value }))
 	}
 
 	//#endregion
 	// 
 
+	// 
+	//#region Login on Submit
+
+	const [loginErrors, setErrors] = useState<string[]>([])
 
 	const onSubmitLogin = async (e : React.FormEvent) => {
 		e.preventDefault()
+		setErrors([])
+		setIsLoading(true)
+
 		const auth = await SignInApi(loginData)
+
+		setIsLoading(false)
+
+		if(typeof(auth) === "string"){
+			setErrors([auth])
+			return
+		}
+
 		if(auth){
 			navigate("/user", {replace : true})
 		}
 	}
+
+	//#endregion
+	// 
 
 
 	return (
@@ -58,7 +78,9 @@ const LandingPage = () => {
 							name='email'
 							onChange={onChangeLogin}
 							value={loginData.email}
+							disabled={isLoading}
 							placeholder='Email'/>
+						
 						<Input.Text
 							required
 							label='Password'
@@ -66,33 +88,32 @@ const LandingPage = () => {
 							onChange={onChangeLogin}
 							value={loginData.password}
 							type='password'
+							disabled={isLoading}
 							placeholder='Password'/>
+						<BulletList.Error
+							hide={loginErrors.length === 0}
+							items={loginErrors}/>
 						<div className='my-4 flex gap-4 justify-center'>
 							<Button.Action
 								type='submit'
+								disabled={isLoading}
 								className='flex-grow'>
 								LOGIN
 							</Button.Action>
 							<Button
+								disabled={isLoading}
 								className='flex-grow'>
 								SIGN UP
 							</Button>
 						</div>
 						<div className='m-4 flex gap-4 justify-center'>
-							<AuthProvider.Facebook onAuth={(e)=>{
-								console.log("test2",e)
-							}}/>
-							<AuthProvider.Google onAuth={(e)=>{
-								console.log("test3",e)
-							}}/>
-							<AuthProvider.Twitter onAuth={(e)=>{
-								console.log("test4",e)
-							}}/>
-							<AuthProvider.Github onAuth={(e)=>{
-								console.log("test5",e)
-							}}/>
+							{/* <AuthProvider.Facebook onAuth={()=>{}}/> */}
+							<AuthProvider.Google onAuth={()=>{}}/>
+							{/* <AuthProvider.Twitter onAuth={()=>{}}/> */}
+							{/* <AuthProvider.Github onAuth={()=>{}}/> */}
 						</div>
 					</form>
+					
 				</Section.Blur>
 			</div>
 		</div>
