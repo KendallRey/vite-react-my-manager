80e144aaa3b920c82d69c094df6b98b3c80005ad
UVMapping <107897863+KendallRey@users.noreply.github.com>
2024-03-25 09:58:30 +0800

    update file indentation to tabs

--- src/context/preference.tsx
+++ src/context/preference.tsx
@@ -4,21 +4,21 @@ import { useCookies } from 'react-cookie';
 import { GetIDs, ReplaceObjectWithID } from "@/helpers/array-helper";
 
 export const PreferenceContext = createContext<PreferenceContextType>({
-  gihubInfo: null,
-  savePreference() {},
-  clearPreference() {},
-  savedRepos: [],
-  repos: [],
-  saveRepos(_) {},
-  clearRepos() {},
-  addSavedRepo(_) {},
-  removeSavedRepo(_) {},
-  loadSavedRepos() {},
-  clearLoadedSavedRepos() {},
+	gihubInfo: null,
+	savePreference() {},
+	clearPreference() {},
+	savedRepos: [],
+	repos: [],
+	saveRepos(_) {},
+	clearRepos() {},
+	addSavedRepo(_) {},
+	removeSavedRepo(_) {},
+	loadSavedRepos() {},
+	clearLoadedSavedRepos() {},
 });
 
 type PreferenceProviderType = {
-  children: React.ReactNode;
+	children: React.ReactNode;
 };
 
 export const GITHUB_INFO = 'github-info'
@@ -26,51 +26,51 @@ export const SAVED_REPOS = 'saved-repos'
 
 const PreferenceProvider: React.FC<PreferenceProviderType> = (props) => {
 
-  const { children } = props;
-  const [cookies, setCookies, removeCookie] = useCookies([GITHUB_INFO, SAVED_REPOS]);
+	const { children } = props;
+	const [cookies, setCookies, removeCookie] = useCookies([GITHUB_INFO, SAVED_REPOS]);
 
-  // #region Token and Org
+	// #region Token and Org
 
-  const SavePreference = (userInfo?: UserGithubInfo) => {
+	const SavePreference = (userInfo?: UserGithubInfo) => {
 
-    const newInfo: UserGithubInfo = {
-      token: userInfo?.token ?? '',
-      orgName: userInfo?.orgName ?? '',
-    }
+		const newInfo: UserGithubInfo = {
+			token: userInfo?.token ?? '',
+			orgName: userInfo?.orgName ?? '',
+		}
 
-    const stringifiedUser = JSON.stringify(newInfo);
-    setCookies(GITHUB_INFO, stringifiedUser, {
-      path: '/',
-    });
-  }
+		const stringifiedUser = JSON.stringify(newInfo);
+		setCookies(GITHUB_INFO, stringifiedUser, {
+			path: '/',
+		});
+	}
 
-  const ClearPreference = () => {
-    removeCookie(GITHUB_INFO);
-  }
+	const ClearPreference = () => {
+		removeCookie(GITHUB_INFO);
+	}
 
-  const GithubInfo = useMemo(() => {
-    try {
-      return cookies[GITHUB_INFO] ?? null;
-    } catch (e) {
-      return null;
-    }
-  }, [cookies[GITHUB_INFO]]);
+	const GithubInfo = useMemo(() => {
+		try {
+			return cookies[GITHUB_INFO] ?? null;
+		} catch (e) {
+			return null;
+		}
+	}, [cookies[GITHUB_INFO]]);
 
-  //#endregion
+	//#endregion
 
-  //#region Repos
+	//#region Repos
 
-  const [repos, setRepos] = useState<SavedRepo[]>([])
+	const [repos, setRepos] = useState<SavedRepo[]>([])
 
-  const SaveRepos = (repos: string[]) => {
-    setCookies(SAVED_REPOS, repos, {
-      path: '/',
-    });
-  }
+	const SaveRepos = (repos: string[]) => {
+		setCookies(SAVED_REPOS, repos, {
+			path: '/',
+		});
+	}
 
-  const AddSavedRepo = (repoConfig: SavedRepo) => {
+	const AddSavedRepo = (repoConfig: SavedRepo) => {
 		const savedRepoIDs = GetIDs(SavedRepos);
-    const addNewRepo = !savedRepoIDs.includes(repoConfig.id);
+		const addNewRepo = !savedRepoIDs.includes(repoConfig.id);
 		let _repos = SavedRepos ?? [];
 
 		if(addNewRepo) 
@@ -78,55 +78,55 @@ const PreferenceProvider: React.FC<PreferenceProviderType> = (props) => {
 		else 
 			_repos = ReplaceObjectWithID(SavedRepos, repoConfig.id, repoConfig);
 
-    setCookies(SAVED_REPOS, _repos, {
-      path: '/',
-    });
-  }
+		setCookies(SAVED_REPOS, _repos, {
+			path: '/',
+		});
+	}
 
-  const RemoveSavedRepo = (repo: string) => {
-    const _repos = SavedRepos ? SavedRepos.filter((item) => item.id !== repo) : []
-    setCookies(SAVED_REPOS, _repos, {
-      path: '/',
-    });
-  }
+	const RemoveSavedRepo = (repo: string) => {
+		const _repos = SavedRepos ? SavedRepos.filter((item) => item.id !== repo) : []
+		setCookies(SAVED_REPOS, _repos, {
+			path: '/',
+		});
+	}
 
-  const LoadSavedRepos = () => {
-    setRepos(SavedRepos ?? [])
-  }
+	const LoadSavedRepos = () => {
+		setRepos(SavedRepos ?? [])
+	}
 
-  const ClearLoadedSavedRepos = () => setRepos([])
+	const ClearLoadedSavedRepos = () => setRepos([])
 
-  const ClearRepos = () => {
-    removeCookie(SAVED_REPOS);
-  }
+	const ClearRepos = () => {
+		removeCookie(SAVED_REPOS);
+	}
 
-  const SavedRepos = useMemo(() => {
-    try {
-      return cookies[SAVED_REPOS] as SavedRepo[] ?? [];
-    } catch (e) {
-      return [];
-    }
-  }, [cookies[SAVED_REPOS]]);
+	const SavedRepos = useMemo(() => {
+		try {
+			return cookies[SAVED_REPOS] as SavedRepo[] ?? [];
+		} catch (e) {
+			return [];
+		}
+	}, [cookies[SAVED_REPOS]]);
 
-  //#endregion
+	//#endregion
 
-  return (
-    <PreferenceContext.Provider value={{
-      gihubInfo: GithubInfo,
-      savePreference: SavePreference,
-      clearPreference: ClearPreference,
-      saveRepos: SaveRepos,
-      clearRepos: ClearRepos,
-      repos: repos,
-      savedRepos: SavedRepos,
-      addSavedRepo: AddSavedRepo,
-      removeSavedRepo: RemoveSavedRepo,
-      loadSavedRepos: LoadSavedRepos,
-      clearLoadedSavedRepos: ClearLoadedSavedRepos
-    }}>
-      {children}
-    </PreferenceContext.Provider>
-    )
+	return (
+		<PreferenceContext.Provider value={{
+			gihubInfo: GithubInfo,
+			savePreference: SavePreference,
+			clearPreference: ClearPreference,
+			saveRepos: SaveRepos,
+			clearRepos: ClearRepos,
+			repos: repos,
+			savedRepos: SavedRepos,
+			addSavedRepo: AddSavedRepo,
+			removeSavedRepo: RemoveSavedRepo,
+			loadSavedRepos: LoadSavedRepos,
+			clearLoadedSavedRepos: ClearLoadedSavedRepos
+		}}>
+			{children}
+		</PreferenceContext.Provider>
+		)
 
 }
 
--- src/helpers/array-helper.ts
+++ src/helpers/array-helper.ts
@@ -1,23 +1,23 @@
 import { v4 as uuidv4 } from 'uuid';
 
 type WithID = {
-  id: string
+	id: string
 }
 
 export const GetIDs = (list: WithID[]) => list.map((item) => item.id);
 
 export const SetRandomIDs = <T>(list: T[]) => list.map((item) => {
-  return {
-    id: uuidv4(),
-    ...item,
-  } as (WithID & T)
+	return {
+		id: uuidv4(),
+		...item,
+	} as (WithID & T)
 })
 
 export const ReplaceObjectWithID = <T>(list: (T & WithID)[], id: string, obj: T) => list.map((item) => {
-  if(item.id === id)
-    return {
-      id,
-      ...obj
-    }
-  return item;
+	if(item.id === id)
+		return {
+			id,
+			...obj
+		}
+	return item;
 })
